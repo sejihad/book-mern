4556ae44c87bf8546424e92b0196c037011a9c56
SE JIHAD <ijihad639@gmail.com>
2025-07-19 22:46:06 +0600

    e

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,9 @@ cloudinary.config({
 });
 
 const PORT = process.env.PORT || 3000;
-
+app.get("/", (req, res) => {
+  res.send("API IS WORKING");
+});
 const server = app.listen(PORT, () => {
   console.log(`🚀 Server is running at http://localhost:${PORT}`);
 });
